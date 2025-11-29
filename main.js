d961efbda115fbcd1d22d311f4ea74c9924bc01e
suganyaneena <suganyapandi07@gmail.com>
2025-11-29 19:12:33 +0530

    feat: app structure

---
+++ main.js
@@ -0,0 +1,13 @@
+import express from "express";
+const app = express()
+const POST  = 6969;
+app.get('/', (req,res) => {
+    res.json({msg: "Hello students"})
+})
+
+app.listen(6969, () => {
+    console.log(`the server is running at http://localhost:${6969}`);
+});
+
+// DRY Principle
+// KISS Principle
