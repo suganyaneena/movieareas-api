ab4052472e74fdb75e83d81e24ee55c8e6012ce1
suganyaneena <suganyapandi07@gmail.com>
2025-11-29 21:27:56 +0530

    route structure

--- main.js
+++ main.js
@@ -1,9 +1,34 @@
 import express from "express";
 const app = express()
 const POST  = 6969;
+
+
 app.get('/', (req,res) => {
     res.json({msg: "Hello students"})
 })
+
+// CRUD functionality of movies
+
+// R - For Reading
+app.get('/movies', () => {
+
+})
+
+// C - For Creating movies
+app.get('/movies', () => {
+
+})
+
+// U - For Updating movies
+app.get('/movies/:id', () => {
+
+})
+
+// D - For Deleting movies
+app.get('/movies/:id', () => {
+
+})
+
 
 app.listen(6969, () => {
     console.log(`the server is running at http://localhost:${6969}`);
